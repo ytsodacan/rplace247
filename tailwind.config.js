bec9b353c92064ee81e122d1f5712f4e8b44a301
afunyun <afunyun@gmail.com>
2025-07-07 19:04:22 -0500

    integrate Tailwind CSS with PostCSS configuration and styling

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./public/**/*.js",
+    "./src/**/*.js",
+    "./timer/**/*.html",
+    "./timer/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
